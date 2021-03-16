6ba09188de2dd7747b2b58a2273dd18e3eb3d905
Kent Zamudio <kazamudio@up.edu.ph>
2021-03-16 12:35:43 +0800

    Modified runkotlin.sh

--- runkotlin.sh
+++ runkotlin.sh
@@ -3,23 +3,24 @@ FILE=$2
 
 cd $DIR
 
-echo
-echo "compiling ${FILE}.kt to ${FILE}.jar file..."
+echo "Compiling ${FILE}.kt to ${FILE}.jar file..."
 kotlinc "${FILE}.kt" -include-runtime -d "${FILE}.jar"
-echo 
 
 if [[ $? -ne 0 ]]
-then
+then (
+	echo
 	echo "Compilation failed..."
-	echo 
-else
+	echo )
+else (
+	echo
 	echo "Successfully created ${FILE}.jar file..."
 	echo "Executing ${FILE}.jar file..."	
 	echo 
 	java -jar ${FILE}.jar
 	echo 
+	sleep 3
 	echo "End of execution..."
-	echo "Terminating..."
+	echo "Terminated...")
 fi
 
 
